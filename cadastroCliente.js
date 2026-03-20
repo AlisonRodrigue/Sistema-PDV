483478b05597a30287f6dcc99bb2804c31013a43
Alison <alisonpereira798@gmail.com>
2026-03-20 17:07:13 -0300

    Adicionando a tela de login e fazendo metodo post no back para fazer o login funcionar

--- cadastroCliente.js
+++ cadastroCliente.js
@@ -3,16 +3,44 @@ const url = 'http://localhost:3000/usuarios'
 async function cadastro() {
     const nome = document.getElementById('nomeCliente').value;
     const email = document.getElementById('emailCliente').value;
-    const senha = document.getElementById('senhaCliente').value;
+    const senha1 = document.getElementById('senhaCliente1').value;
+    const senha2 = document.getElementById('senhaCliente2').value;
+    const spanDiferente = document.getElementById('spanDiferentes');
+    const spanVazio = document.getElementById('spanVazio');
+    const spanMenor = document.getElementById('spanMenor');
 
+    spanMenor.style.display= 'none'
+    spanDiferente.style.display= 'none';
+    spanVazio.style.display='none';
+
+
+    if(senha1.length <8 || senha2.length <8){
+        spanMenor.style.display= 'block';
+        return
+    }
+    if(senha1 === '' || senha2 === ''){
+        spanVazio.style.display='block';
+        document.getElementById('senhaCliente1').focus();
+        return
+    }
+    if(senha1 != senha2){ 
+        spanDiferente.style.display = 'block';
+        document.getElementById('senhaCliente1').value = '';
+        document.getElementById('senhaCliente2').value = '';
+        return;
+    }
     await fetch(url, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json'},
         body: JSON.stringify({
             nome,
             email,
-            senha
+            senha: senha1
         })
     });
     alert ("Cliente cadastrado com sucesso");
+}
+
+async function Login() {
+    
 }
--- node.js
+++ node.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
+const cors = require('cors');
 
-
+app.use(cors());
 app.use(express.json());
 
 
@@ -13,23 +14,42 @@ const db = mysql.createConnection({
     database: 'mercearia_db'
 });
 
-app.post('/usuarios', (req, res)=>{
-    const {nome, email , senha}= req.body;
-    const sql= "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
+app.post('/usuarios', (req, res) => {
+    const { nome, email, senha } = req.body;
+    const sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
 
-    db.query(sql, [nome,email,senha], (err, results)=>{
-        if(err) return res.status(500).send(err);
+    db.query(sql, [nome, email, senha], (err, results) => {
+        if (err) return res.status(500).send(err);
         res.send('Usuário cadastrado com sucesso!');
     })
 })
 
-app.get('/usuarios', (req, res)=>{
-    db.query('SELECT * FROM usuarios', (err, results)=>{
-        if(err) return res.status(500).send(err);
+app.get('/usuarios', (req, res) => {
+    db.query('SELECT * FROM usuarios', (err, results) => {
+        if (err) return res.status(500).send(err);
         res.json(results);
     })
 })
+app.post('/login', (req, res) => {
+    const { email, senha } = req.body;
+    const sql = "SELECT * FROM usuarios where email= ? AND senha= ?";
+    db.query(sql, [email, senha], (err, results) => {
+        if (err) return res.status(500).send(err);
+        if (results.length > 0) {
+            const respostaDeSucesso ={
+                mensagem: "Login realizado com sucesso!",
+                usuario: {
+                    id: results[0].id,
+                    nome: results[0].nome
+        }
+            }
+            res.json(respostaDeSucesso);
+        } else {
+            res.status(404).json({error: "E-mail ou senha incorretos"});
+        }
+    });
+})
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('🚀 Servidor rodando em http://localhost:3000')
 })
